7c8e8cdc2342428e795c79201caae86058de5c4c
Willie Soedewa <willie.soedewa12@gmail.com>
2022-09-09 11:45:07 +0800

    FIX: visit is per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :lists
+  has_many :bookings
   has_many :favorites, through: :lists
 end
